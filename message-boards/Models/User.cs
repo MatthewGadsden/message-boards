d13bde4512e0f31aa17543bf703307bddf47ff7e
MatthewGadsden <matthewjgadsden@gmail.com>
2025-06-12 19:26:40 +1000

    feat: basic user structure

---
+++ message-boards/Models/User.cs
@@ -0,0 +1,10 @@
+namespace message_boards.Models;
+
+public class User
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    
+    // users that this user follows ids
+    private HashSet<int> FollowingUserIds { get; set; } = [];
+}
